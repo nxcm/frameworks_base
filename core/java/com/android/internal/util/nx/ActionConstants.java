fd09b64e9c3329d7abcc3b39d67798d524666fd8
Lars Greiss <kufikugel@googlemail.com>
2014-12-08 23:39:21 -0200

    Frameworks: inital bringup of slim actions and helper classes Bring back slimactions.
    - Most actions are available and adapted to LP (mainly statusbar actions we did on kk need to be ported back again)
    - Cleanup of old code
    - Renamed classes and methods
    
    and more.....
    
    This is the base for all the comming features now

--- core/java/com/android/internal/util/nx/ActionConstants.java
+++ core/java/com/android/internal/util/nx/ActionConstants.java
@@ -16,7 +16,7 @@
 
 package com.android.internal.util.nx;
 
-public class nxActionConstants {
+public class ActionConstants {
 
     // key must fit with the values arrays from Settings to use
     // SlimActions.java actions
--- core/java/com/android/internal/util/nx/DeviceUtils.java
+++ core/java/com/android/internal/util/nx/DeviceUtils.java
@@ -127,11 +127,11 @@ public class DeviceUtils {
     }
 
     private static boolean isSupportedFeature(Context context, String action) {
-        if (action.equals(nxActionConstants.ACTION_TORCH)
+        if (action.equals(ActionConstants.ACTION_TORCH)
                         && !deviceSupportsTorch(context)
-                || action.equals(nxActionConstants.ACTION_VIB)
+                || action.equals(ActionConstants.ACTION_VIB)
                         && !deviceSupportsVibrator(context)
-                || action.equals(nxActionConstants.ACTION_VIB_SILENT)
+                || action.equals(ActionConstants.ACTION_VIB_SILENT)
                         && !deviceSupportsVibrator(context)) {
             return false;
         }
